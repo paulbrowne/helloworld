d56b0cfedd2d747b2ab6c6c1e926168969ab6f84
Paul Browne <paulfj.browne@gmail.com>
2015-10-20 10:59:18 +0100

    Adding a new line to test automatic build on Heidi instead of Alfred

--- helloworld.c
+++ helloworld.c
@@ -7,5 +7,5 @@ main()
         printf("Hello World! \n");
         printf("New line for a new commit \n");
         printf("Let's add another one \n");
-        printf("Let's do this all the live-long day! \n");
+        printf("Adding a new line to test GitHub webhook on Heidi instead of Alfred \n");
     }
