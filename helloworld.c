5169ae46f8576258cf5475464b0d2286faf771f9
Paul Browne <paulfj.browne@gmail.com>
2016-02-11 10:40:04 +0000

    Removed a line to trigger a built

--- helloworld.c
+++ helloworld.c
@@ -6,6 +6,5 @@ main()
     {
         printf("Hello World! \n");
         printf("New line for a new commit \n");
-        printf("Let's add another one \n");
         printf("Adding a new line to test GitHub webhook on Heidi instead of Alfred \n");
     }
